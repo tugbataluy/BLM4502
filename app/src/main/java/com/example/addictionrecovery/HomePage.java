e41bd70f4f6219ed77042d27f05a9e2164d2a374
tugba taluy <tugbataluy@hotmail.com>
2024-04-11 21:45:10 +0300

    home page drawer menu created

--- app/src/main/java/com/example/addictionrecovery/HomePage.java
+++ app/src/main/java/com/example/addictionrecovery/HomePage.java
@@ -1,15 +1,70 @@
 package com.example.addictionrecovery;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity {
 
-
+    RelativeLayout homePageLayout;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView navIcon;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        homePageLayout= (RelativeLayout)findViewById(R.id.homepage_layout);
+        navigationView=(NavigationView) findViewById(R.id.nav_view);
+        navigationView.setVisibility(View.INVISIBLE);
+        drawerLayout= (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        navIcon=(ImageView) findViewById(R.id.navigation_icon);
+        navIcon.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(navigationView.getVisibility()==View.INVISIBLE){
+                    navigationView.setVisibility(View.VISIBLE);
+                }
+                else{
+                    navigationView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        homePageLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(navigationView.getVisibility()==View.VISIBLE){
+                    navigationView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
