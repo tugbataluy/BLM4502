9a49e69b064cce75317d240008037bcaafeaf5cb
tugba taluy <tugbataluy@hotmail.com>
2024-05-02 17:23:55 +0300

    logout dialog added subs addiction pages

--- app/src/main/java/com/example/addictionrecovery/SubstanceAddictionSupportPage.java
+++ app/src/main/java/com/example/addictionrecovery/SubstanceAddictionSupportPage.java
@@ -1,9 +1,11 @@
 package com.example.addictionrecovery;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -81,7 +83,7 @@ public class SubstanceAddictionSupportPage extends AppCompatActivity {
                         startActivity(intent3);
                         break;
                     case R.id.cikis_yap_option:
-                        logout();
+                        showLogoutConfirmationDialog();
                         break;
                     default:
                         return true;
@@ -92,6 +94,28 @@ public class SubstanceAddictionSupportPage extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    private void showLogoutConfirmationDialog() {
+        // AlertDialog oluştur
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Çıkış yapmak istediğinize emin misiniz?");
+        builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // İptal durumu, bir şey yapmaya gerek yok
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
     public void logout(){
         auth.signOut();
--- app/src/main/java/com/example/addictionrecovery/SubstanceAddictionVideosPage.java
+++ app/src/main/java/com/example/addictionrecovery/SubstanceAddictionVideosPage.java
@@ -1,9 +1,11 @@
 package com.example.addictionrecovery;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -78,7 +80,7 @@ public class SubstanceAddictionVideosPage extends AppCompatActivity {
                         startActivity(intent3);
                         break;
                     case R.id.cikis_yap_option:
-                        logout();
+                        showLogoutConfirmationDialog();
                         break;
                     default:
                         return true;
@@ -89,6 +91,25 @@ public class SubstanceAddictionVideosPage extends AppCompatActivity {
 
             }
         });
+    }
+    private void showLogoutConfirmationDialog() {
+        // AlertDialog oluştur
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Çıkış yapmak istediğinize emin misiniz?");
+        builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // İptal durumu, bir şey yapmaya gerek yok
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
     public void logout(){
         auth.signOut();
