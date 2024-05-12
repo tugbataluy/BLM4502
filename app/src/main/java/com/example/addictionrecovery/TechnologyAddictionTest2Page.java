353ebb5129301154a2e76662392373d4754d9674
Tuğba Oğuz <tugba.oguz.5428@gmail.com>
2024-05-12 21:21:19 +0300

    Tech test pages and layouts are updated

--- app/src/main/java/com/example/addictionrecovery/TechnologyAddictionTest2Page.java
+++ app/src/main/java/com/example/addictionrecovery/TechnologyAddictionTest2Page.java
@@ -1,23 +1,45 @@
 package com.example.addictionrecovery;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TechnologyAddictionTest2Page extends AppCompatActivity {
 
+    RelativeLayout relativeLayout;
+    FirebaseAuth auth;
+    Toolbar tb;
+    NavigationView navigationView;
+    ImageView navIcon;
+    BottomNavigationView bottomNavigationView;
     public List<QuizModel> questionList;
     TextView tvQuestion, tvQuestionNo;
     RadioGroup radioGroup;
@@ -31,6 +53,16 @@ public class TechnologyAddictionTest2Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_technology_addiction_test2_page);
+
+        auth= FirebaseAuth.getInstance();
+        relativeLayout=(RelativeLayout) findViewById(R.id.my_test_relative_layout);
+
+        setToolbarTitle();
+        backButtonActivity();
+        drawerInitialization();
+        navBottomArrangements();
+        getUserName(userName -> setUserName(userName));
+        relativeLayoutClickerEnable();
 
         questionList = new ArrayList<>();
         tvQuestion = findViewById(R.id.questionTitle);
@@ -140,6 +172,191 @@ public class TechnologyAddictionTest2Page extends AppCompatActivity {
             dialog.show();
             //finish();
         }
+    }
+
+    public void setToolbarTitle(){
+        tb=(Toolbar) findViewById(R.id.toolbar);
+        tb.setTitle("Teknoloji Bağımlılığı");
+    }
+
+    public void backButtonActivity(){
+        OnBackPressedDispatcher onBackPressedDispatcher = getOnBackPressedDispatcher();
+        onBackPressedDispatcher.addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                // Yeni aktiviteye geçmek için Intent oluştur
+                Intent intent = new Intent(TechnologyAddictionTest2Page.this, TechnologyAddictionMainPage.class);
+                // Yeni aktiviteyi başlat
+                startActivity(intent);
+                // Mevcut aktiviteyi sonlandır
+                finish();
+            }
+        });
+    }
+
+    public void logout(){
+        auth.signOut();
+
+        // Giriş sayfasına yönlendir
+        Intent intent = new Intent(TechnologyAddictionTest2Page.this, LoginPage.class);
+        startActivity(intent);
+        finish(); // Bu aktiviteyi kapat
+    }
+
+    private void showLogoutConfirmationDialog() {
+        // AlertDialog oluştur
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Çıkış yapmak istediğinize emin misiniz?");
+        builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // İptal durumu, bir şey yapmaya gerek yok
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void getUserName(final TechnologyAddictionMainPage.OnUserNameFetchedListener listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        DocumentReference docRef = db.collection("users").document(user.getUid());
+
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document != null && document.exists()) {
+                    String name = document.getString("Name");
+                    if (name != null) {
+                        Log.d("drawer_menu", "Name alanı not null.");
+                        // İsim başarıyla alındı, dinleyiciye iletilir.
+                        listener.onUserNameFetched(name);
+                    } else {
+                        Log.d("drawer_menu", "Name alanı null.");
+                        listener.onUserNameFetched(null);
+                    }
+                } else {
+                    Log.d("drawer_menu", "Doküman bulunamadı");
+                    listener.onUserNameFetched(null);
+                }
+            } else {
+                Log.d("drawer_menu", "Belge alınamadı: ", task.getException());
+                listener.onUserNameFetched(null);
+            }
+        });
+    }
+
+    public void setUserName(String userName) {
+        if (userName != null) {
+            TextView user_name = findViewById(R.id.user_name);
+            user_name.setText(userName);
+        }
+    }
+
+    public void drawerInitialization(){
+        navigationView=(NavigationView) findViewById(R.id.nav_view);
+        navigationView.setVisibility(View.INVISIBLE);
+        navIcon=(ImageView) findViewById(R.id.navigation_icon);
+
+        navIcon.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(navigationView.getVisibility()==View.INVISIBLE){
+                    navigationView.setVisibility(View.VISIBLE);
+                }
+                else{
+                    navigationView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.kullanici_profili_option:
+                        Intent intent= new Intent(TechnologyAddictionTest2Page.this,user_profile.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.neyi_amacliyoruz_option:
+                        Intent intent1=new Intent (TechnologyAddictionTest2Page.this, Purpose.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.geri_bildirim_option:
+                        Intent intent3 = new Intent(TechnologyAddictionTest2Page.this, Feedback.class);
+                        startActivity(intent3);
+                        break;
+
+                    case R.id.cikis_yap_option:
+                        showLogoutConfirmationDialog();
+                        break;
+                    default:
+                        return true;
+
+
+                }
+                return  false;
+
+            }
+        });
+
+    }
+
+    public void navBottomArrangements(){
+        bottomNavigationView=(BottomNavigationView) findViewById(R.id.bottom_nav_id);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId= item.getItemId();
+                switch (itemId){
+                    case R.id.home_tab:
+                        System.out.println("home");
+                        Intent intent= new Intent(TechnologyAddictionTest2Page.this,TechnologyAddictionMainPage.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.questions_tab:
+                        System.out.println("questions");
+                        Intent intent2= new Intent(TechnologyAddictionTest2Page.this,TechnologyAddictionQuestionsPage.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.videos_tab:
+                        System.out.println("videos");
+                        Intent intent3= new Intent(TechnologyAddictionTest2Page.this,TechnologyAddictionVideosPage.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.support_tab:
+                        System.out.println("support");
+                        Intent intent4= new Intent(TechnologyAddictionTest2Page.this,TechnologyAddictionSupportPage.class);
+                        startActivity(intent4);
+                        break;
+                    default:
+
+                        break;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    public void relativeLayoutClickerEnable(){
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(navigationView.getVisibility()==View.VISIBLE){
+                    navigationView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
 
